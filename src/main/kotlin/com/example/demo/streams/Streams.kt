c1fe06212e76e01c5521d4e6982e33a2cbda269c
illenko <kostiaillenko@gmail.com>
2025-01-30 10:01:35 +0200

    Fix state store

--- src/main/kotlin/com/example/demo/streams/Streams.kt
+++ src/main/kotlin/com/example/demo/streams/Streams.kt
@@ -8,6 +8,7 @@ import org.apache.kafka.common.serialization.Serdes
 import org.apache.kafka.common.utils.Bytes
 import org.apache.kafka.streams.StreamsBuilder
 import org.apache.kafka.streams.kstream.Consumed
+import org.apache.kafka.streams.kstream.Grouped
 import org.apache.kafka.streams.kstream.Materialized
 import org.apache.kafka.streams.state.KeyValueStore
 import org.springframework.stereotype.Component
@@ -20,7 +21,9 @@ class Streams : KafkaStreamsStarter() {
                 .stream("payments", Consumed.with<String, Payment>(Serdes.String(), SerdesUtils.getValueSerdes()))
                 .selectKey { _, it -> it.userId }
 
-        payments.groupByKey().aggregate(
+        payments.groupByKey(
+            Grouped.with(Serdes.String(), SerdesUtils.getValueSerdes())
+        ).aggregate(
             {
                 UserPayments
                     .newBuilder()
